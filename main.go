01c61275cc95528d6b46e751dd9dd989afec1fe1
adrianhm13 <adrian.hervasmartinez@gmail.com>
2024-02-24 17:56:34 +0100

    refactor to use constants for http methods

--- main.go
+++ main.go
@@ -14,10 +14,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
